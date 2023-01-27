e8cbb179c531291d57747a9bcffa83c29753cb4a
DiegoVillasenorVilla <divivi.619@gmail.com>
2023-01-27 14:36:17 -0600

    responsive desing added to body, hard skills and image hover components

--- src/components/hard skills/HardSkills.tsx
+++ src/components/hard skills/HardSkills.tsx
@@ -5,32 +5,32 @@ export const HardSkills = () => {
     return (
         <section className="hard-skills_section">
         <div className="hard-skill_container">
-          <p>HTML</p>
-          <span>2 years Experience</span>
+          <p className="skill-paragraph">HTML</p>
+          <p className="description-paragraph">2 years Experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>CSS</p>
-          <span>2 years Experience</span>
+          <p className="skill-paragraph">CSS</p>
+          <p className="description-paragraph">2 years Experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>React</p>
-          <span>1 year of experience</span>
+          <p className="skill-paragraph">React</p>
+          <p className="description-paragraph">1 year of experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>Javascript</p>
-          <span>2 years Experience</span>
+          <p className="skill-paragraph">Javascript</p>
+          <p className="description-paragraph">2 years Experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>Typescript</p>
-          <span>1 year of experience</span>
+          <p className="skill-paragraph">Typescript</p>
+          <p className="description-paragraph">1 year of experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>Git</p>
-          <span>1 year of experience</span>
+          <p className="skill-paragraph">Git</p>
+          <p className="description-paragraph">1 year of experience</p>
         </div>
         <div className="hard-skill_container">
-          <p>SASS</p>
-          <span>1 year of experience</span>
+          <p className="skill-paragraph">SASS</p>
+          <p className="description-paragraph">1 year of experience</p>
         </div>
       </section>
     );
